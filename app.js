ec41bf05cbfb490410f35868a8b7fb8744cf4ffc
elaheerazob <ridwanelahee282@gmail.com>
2022-02-28 14:32:43 +0600

    add

--- app.js
+++ app.js
@@ -2,17 +2,34 @@ const searchFeld = () =>{
     const inputFeld = document.getElementById('input-feld');
     const inputFeldValue =inputFeld.value;
     // console.log(inputFeldValue);
-    inputFeld.value ='';
-
-    const url =`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputFeldValue}`
+    toggleSpinner('block')
+    if(inputFeld.value == ''){
+        alert('Felds Empty')
+    }else{
+        const url =`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputFeldValue}`
     fetch(url)
     // console.log(url);
     .then(res => res.json())
     .then(data => displayDrink(data.drinks))
+    }
+
+    
+    inputFeld.value ='';
 }
+//spinner 
+ const toggleSpinner = (style) =>{
+    const spinner =document.getElementById('spinner');
+    spinner.style.display =(style)
+ }
+
+
+//display drink 
 const displayDrink = drink =>{
     // console.log(drink);
-    const cocktelShow =document.getElementById('cocktel-show');
+    if(drink ==null){
+        alert('No result Fund ')
+    }else{
+        const cocktelShow =document.getElementById('cocktel-show');
     cocktelShow.textContent ='';
     drink.forEach(drinks => {
             // console.log(drinks);
@@ -20,19 +37,31 @@ const displayDrink = drink =>{
         const div =document.createElement('div');
         div.classList.add('card','col-6','bg-secondary')
         div.innerHTML =`
-                    <div class="text-white  " style="width: 18rem;">
+                    <div id="myCard" class="text-white  " style="width: 18rem;">
                         <img src="${drinks.strDrinkThumb}" class=" card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${drinks.strDrink}</h5>
                              <p class="card-text">${drinks.strInstructions.slice(0,150)}</p>
                             
                             <button onclick="showDetails('${drinks.idDrink}')" class="btn btn-info">More Details</button>
+                            <button onclick="deleteButton('${drinks}')" class="btn btn-danger">Delete</button>
                         </div>
                     </div>`;
         cocktelShow.appendChild(div);          
 
     });
+    toggleSpinner('none')
+    }
+    
 }
+//Delete button
+const deleteButton = () =>{
+    const myCard =document.getElementById('myCard')
+    myCard.style.display ='none'
+}
+
+
+//showDetails button
 const showDetails = id =>{
     // console.log(id);
     const url =`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
@@ -47,9 +76,9 @@ const detailsisShow = detail =>{
     cocktailDetaileShow.textContent ='';
     // cocktailDetaileShow.textContent ='';
     const div =document.createElement('div')
-    div.classList.add('card')
+    div.classList.add('card','col-6','mx-auto')
     div.innerHTML =`
-                <div class="card" style="width: 18rem;">
+                <div  style="width: 18rem;">
                     <img src="${detail.strDrinkThumb}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${detail.strDrink}</h5>
